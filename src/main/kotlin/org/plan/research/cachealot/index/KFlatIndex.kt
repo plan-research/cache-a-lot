d0e6c663f771f84c276097cd0146b5e2778ece0b
Rustam Sadykov <sbonekenobi@gmail.com>
2024-05-14 11:05:34 +0200

    add flat index

--- src/main/kotlin/org/plan/research/cachealot/index/KFlatIndex.kt
+++ src/main/kotlin/org/plan/research/cachealot/index/KFlatIndex.kt
@@ -6,6 +6,6 @@ abstract class KFlatIndex<V> : KIndex<Unit, V> {
     abstract suspend fun getCandidates(): Flow<V>
     abstract suspend fun insert(value: V)
 
-    override suspend fun getCandidates(key: Unit): Flow<V> = getCandidates()
-    override suspend fun insert(key: Unit, value: V) = insert(value)
+    final override suspend fun getCandidates(key: Unit): Flow<V> = getCandidates()
+    final override suspend fun insert(key: Unit, value: V) = insert(value)
 }
