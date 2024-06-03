2513ff971870f2d5e4adcc85ea410f79dd8bbaf7
Rustam Sadykov <sbonekenobi@gmail.com>
2024-06-03 15:29:49 +0200

    update stats gathering from events

--- src/main/kotlin/org/plan/research/cachealot/scripts/cache/cache_events_stats_gathering.kt
+++ src/main/kotlin/org/plan/research/cachealot/scripts/cache/cache_events_stats_gathering.kt
@@ -3,121 +3,83 @@ package org.plan.research.cachealot.scripts.cache
 import io.ksmt.solver.KSolver
 import io.ksmt.solver.KSolverStatus
 import kotlinx.coroutines.runBlocking
+import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.api.dataFrameOf
+import org.jetbrains.kotlinx.dataframe.io.writeCSV
 import org.jetbrains.kotlinx.dataframe.math.mean
 import org.jetbrains.kotlinx.dataframe.math.medianOrNull
-import kotlin.io.path.Path
-import kotlin.io.path.createDirectory
-import kotlin.io.path.div
+import java.nio.file.Path
+import kotlin.io.path.*
+import kotlin.properties.Delegates
+import kotlin.time.TimedValue
 
-class CacheEventSubscriberEntry(val name: String) {
-    val candidates = mutableListOf<Int>()
-    val satTime = mutableListOf<Long>()
-    val unsatTime = mutableListOf<Long>()
-    val unknownTime = mutableListOf<Long>()
-    val trueCheckTime = mutableListOf<Long>()
-    val falseCheckTime = mutableListOf<Long>()
-
-    fun add(other: CacheEventSubscriberEntry) {
-        candidates.addAll(other.candidates)
-        satTime.addAll(other.satTime)
-        unsatTime.addAll(other.unsatTime)
-        unknownTime.addAll(other.unknownTime)
-        trueCheckTime.addAll(other.trueCheckTime)
-        falseCheckTime.addAll(other.falseCheckTime)
-    }
-
-    private fun generateString(pref: String, list: List<Int>): String = """
-        ${pref}Min: ${list.minOrNull()}, ${pref}Max: ${list.maxOrNull()}, 
-        ${pref}Mean: ${list.mean()}, ${pref}Med: ${list.medianOrNull()},
-        ${pref}Sum: ${list.sum()}
-    """.trim()
-
-    private fun generateString(pref: String, list: Collection<Long>): String = """
-        ${pref}Min: ${list.minOrNull()}, ${pref}Max: ${list.maxOrNull()}, 
-        ${pref}Mean: ${list.mean()}, ${pref}Med: ${list.medianOrNull()},
-        ${pref}Sum: ${list.sum()}
-    """.trim()
-
-    override fun toString(): String = """
-        $name:
-        
-        ${generateString("candidates", candidates)},
-        
-        ${generateString("satTime", satTime)},
-        
-        ${generateString("unsatTime", unsatTime)},
-        
-        ${generateString("unknownTime", unknownTime)},
-        
-        ${generateString("successSolveTime", satTime + unsatTime)},
-        
-        ${generateString("solveTime", satTime + unsatTime + unknownTime)},
-        
-        ${generateString("trueCheckTime", trueCheckTime)},
-        
-        ${generateString("falseCheckTime", falseCheckTime)},
-        
-        ${generateString("checkTime", trueCheckTime + falseCheckTime)},
-    """.trimIndent()
-}
 
 class CacheEventSubstriber : LinearStatsParserSubscriber {
 
-    val projects = hashMapOf<String, CacheEventSubscriberEntry>()
+    val benchmark = mutableListOf<String>()
+    val smt = mutableListOf<String>()
+    val event = mutableListOf<String?>()
+    val duration = mutableListOf<Long>()
+    val result = mutableListOf<String?>()
 
-    private inline fun withBench(name: String, block: CacheEventSubscriberEntry.() -> Unit) {
-        val bench = name.substringBefore('-')
-        projects.getOrPut(bench) { CacheEventSubscriberEntry(bench) }.block()
+    private fun updateName(name: String) {
+        val benchName = name.substringBefore(' ')
+        val smtName = name.substringAfter(' ', "").takeIf { it.isNotBlank() } ?: smt.last()
+        benchmark += benchName
+        smt += smtName
+    }
+
+    private fun <T> update(parseResult: ParseResult<TimedValue<T>>) {
+        updateName(parseResult.name)
+        val (value, duration) = parseResult.value
+        this.duration += duration.inWholeMilliseconds
+        result += value.toString()
     }
 
     override suspend fun onCheck(parseResult: ParseResultCheck) {
-        withBench(parseResult.name) {
-            val (value, duration) = parseResult.value
-            if (value) {
-                trueCheckTime += duration.inWholeMilliseconds
-            } else {
-                falseCheckTime += duration.inWholeMilliseconds
-            }
-        }
+        event += "check"
+        update(parseResult)
     }
 
     override suspend fun onSolve(parseResult: ParseResultSolve) {
-        withBench(parseResult.name) {
-            val (value, duration) = parseResult.value
-            when (value) {
-                KSolverStatus.SAT -> satTime += duration.inWholeMilliseconds
-                KSolverStatus.UNSAT -> unsatTime += duration.inWholeMilliseconds
-                KSolverStatus.UNKNOWN -> unknownTime += duration.inWholeMilliseconds
-            }
-        }
+        event += "solve"
+        update(parseResult)
     }
 
     override suspend fun onUpdate(parseResult: ParseResultUpdate) {
+        event += "update"
+        updateName(parseResult.name)
+        duration += parseResult.value.inWholeMilliseconds
+        result += null
     }
 
-    override suspend fun onCandidatesConsumed(parseResult: ParseResultCandidatesConsumed) {
-        withBench(parseResult.name) {
-            candidates += parseResult.value
-        }
-    }
+    override suspend fun onCandidatesConsumed(parseResult: ParseResultCandidatesConsumed) {}
+}
 
-    override fun toString(): String = buildString {
-        val all = CacheEventSubscriberEntry("ALL")
-        projects.forEach { (_, value) ->
-            appendLine(value)
-            appendLine()
-            all.add(value)
-        }
-
-        appendLine(all)
+fun compute(path: Path) {
+    val subscriber = CacheEventSubstriber()
+    runBlocking {
+        LinearStatsParser(subscriber).run(path.div("stats.log"))
     }
+    subscriber.run {
+        dataFrameOf(
+            "benchmark" to benchmark,
+            "smt" to smt,
+            "event" to event,
+            "duration" to duration,
+            "result" to result,
+        )
+    }.writeCSV(path.div("stats.csv").toFile())
 }
 
 fun main(args: Array<String>) {
-    val path = Path(args[0]).div("stats.log")
-    val subscriber = CacheEventSubstriber()
-    runBlocking {
-        LinearStatsParser(subscriber).run(path)
+    val path = Path(args[0])
+
+    if (path.div("stats.log").exists()) {
+        compute(path)
+    } else {
+        path.forEachDirectoryEntry {
+            compute(it)
+        }
     }
-    print(subscriber)
 }
